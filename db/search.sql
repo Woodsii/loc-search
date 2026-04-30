2ad2f10bd364067dd04a9507ab2b92f5a53e0f02
Woodsii <beanmanwoods@gmail.com>
2026-04-30 17:06:25 -0400

    make it a logged table (errors without)

--- db/search.sql
+++ db/search.sql
@@ -5,7 +5,7 @@
 -- 245$a$b is title => books.title + title_remainder + books.series.name + volume
 -- 260$c is dateOfPubliation => books.publications.date
 -- 650$a$b$v$x$y$z subject => books.subjects.heading + subheading + general + chron + geo + form
-CREATE UNLOGGED MATERIALIZED VIEW mv_search AS
+CREATE MATERIALIZED VIEW mv_search AS -- removed unlogged, WAL sounds p nice here.
 SELECT
     b.id,
     jsonb_build_object(
